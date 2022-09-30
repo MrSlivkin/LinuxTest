2f241b1cbc94b8417e39d062e9d35cabc200d80a
MrSlivkin <84202197+MrSlivkin@users.noreply.github.com>
2022-09-30 08:08:34 +0500

    Add files via upload

--- hahah.sh
+++ hahah.sh
@@ -4,16 +4,19 @@
 set -x
 
 MAKE_PATH=/mnt/gentoo/etc/portage/make.conf
+#A_L=ACCEPT_LICENSE="-* @FREE"
 
-STAGE_FILE3=$(curl -s http://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds)
+STAGE3_FILE=$(curl -s http://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds)
 STAGE3_URL=https://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds
 
-echo "установка времени"
+
+
+echo "make a time"
 ntpd -q -g
 
 password()
 {
-echo "придумайте пароль root-а"
+echo "Make a password for root"
 passwd
 }
 
@@ -21,22 +24,69 @@ passwd
 partition()
 {
 cd
-parted -a optimal /dev/sda
-mklabel gpt
-unit GB
-mkpart primary 1 2
-name 1 grub
-set 1 bios_grub on
-mkpart primary 2 3
-name 2 boot
-mkpart primary 3 4
-name 3 swap
-mkpart primary 4 -1
-name 4 rootfs
-quit
+DISK=$1
+	P_CHECK=$(lsblk -o NAME,FSTYPE -dSn | grep -o $DISK)
+	if [ "$P_CHECK" == "$DISK" ] ; then
+		echo "hahaha! The YOUR Disk and the Disk on that folder it is the same for you!"
+		echo "still want to continue? (yes/no)"
+		read ANSWER
+		if [ "$ANSWER" == "yes" ] ; then
+			echo "разметка диска"
+			sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' <<- EOF | fdisk /dev/$DISK
+			o # clear the in memory partition table
+			n # new partition
+			p # primary partition
+			1 # partition number 1
+			# default - start at beginning of disk
+			+1GB
+			n # new partition
+			p # primary partition
+			2 # partion number 2
+			# default, start immediately after preceding partition
+			# default, extend partition to end of disk
+			a # make a partition bootable
+			1 # bootable partition is partition 1 -- /dev/sda1
+			p # print the in-memory partition table
+			w # write the partition table
+			q # and we're done
+			EOF
+		else
+			echo "goodbye" ; exit 0
+		fi
+	fi
+	cd
+}
+
+fsys_maker() 
+{
+echo "making file system"
+
+mkfs.vfat /dev/sda1
+mkfs.ext4 /dev/sda2
+
+mkdir /mnt/gentoo/boot
+mount /dev/sda2 /mnt/gentoo
+mount /dev/sda1 /mnt/gentoo/boot
+}
+
+stage3_maker()
+{
+cd
+cd /mnt/gentoo
+echo"unpack st3 archive"
+
+wget $STAGE3_URL/$STAGE3_FILE
+tar xpvf stage3-*.tar.bz2 --xattrs-include='*.*' --numeric-owner
 }
 
 
+DEBUGGER()
+{
+echo "Error: $1"
+exit 1
+}
 
 password
 partition
+fsys_maker || DEBUGGER
+stage3_maker || DEBUGGER
